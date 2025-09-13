27c856bbe5acc6cd2a5f6ea33121e377c4b69747
UnknownFur <123666616+UnknownFur@users.noreply.github.com>
2025-09-13 09:04:13 -0500

    End

--- Imix_Pagina/JAVASCRIPT/PaginaPrincipal.js
+++ Imix_Pagina/JAVASCRIPT/PaginaPrincipal.js
@@ -1,12 +1,3 @@
-function ajustarAltura() {
-  const right = document.querySelector(".right");
-  const img = document.querySelector(".left img");
-
-  if (right && img) {
-    img.style.height = right.offsetHeight + "px";
-  }
-}
-
-// Ajusta al cargar y al redimensionar la ventana
-window.addEventListener("load", ajustarAltura);
-window.addEventListener("resize", ajustarAltura);
+window.addEventListener("load", function() {
+  document.body.classList.add("loaded");
+});
